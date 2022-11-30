65f66c757c1d0a95a6e13d371de22580c76de853
GettingTechnicl <56567784+GettingTechnicl@users.noreply.github.com>
2022-11-30 15:33:51 -0600

    Update SendEmail.ps1

--- SendEmail.ps1
+++ SendEmail.ps1
@@ -1,5 +1,5 @@
-﻿$From = "test@3dmoleculardesigns.com"
-$To = "terrylee23@ymail.com"
+﻿$From = "fromemail"
+$To = "toemail"
 $Subject = "Test Email"
 $Body = "<h2>This is a Test</h2><br><br>"
 $SMTPServer = "us-smtp-outbound-1.mimecast.com"
